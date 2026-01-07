2cdde6820983bd292b5c33d6f343e605bcfb7ae5
Vandan Kacha <codingwithme.18@gmail.com>
2026-01-07 11:11:12 +0530

    fix: save and add ui.dart file

---
+++ lib/ui.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ui extends StatefulWidget {
+  const ui({super.key});
+
+  @override
+  State<ui> createState() => _uiState();
+}
+
+class _uiState extends State<ui> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home Screen"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+    );
+  }
+}
